b1e567b66d759c870e54446839f57912db3a10ce
clxx <Carsten.Lindner@gmx.net>
2023-11-27 02:30:38 +0100

    Fixes

--- vue3-demo/run.py
+++ vue3-demo/run.py
@@ -18,7 +18,11 @@ async def scrape(url):
         page.on("console", lambda message: messages[message.type].append(message.text))
         await page.goto(url)
         await page.screenshot(path="screenshot.png")
-        host_version = await page.locator("#hostVersion").inner_text()
+        host_version = (
+            await page.locator("#hostVersion").inner_text()
+            if await page.locator("#hostVersion").is_visible()
+            else None
+        )
         remote_version = (
             await page.locator("#remoteVersion").inner_text()
             if await page.locator("#remoteVersion").is_visible()
@@ -39,7 +43,11 @@ async def scrape(url):
 
 
 async def serve(
-    host_package_version, remote_package_version, host_shared_hints, remote_shared_hints
+    host_package_version,
+    remote_package_version,
+    host_shared_hints,
+    remote_shared_hints,
+    needs_install,
 ):
     pnpm_lock_yaml = Path("..", "pnpm-lock.yaml")
     pnpm_lock_yaml_bytes = pnpm_lock_yaml.read_bytes()
@@ -75,10 +83,9 @@ async def serve(
     remote_shared_json.write_text(json.dumps(remote_shared_hints, indent=2), "utf-8")
 
     try:
-        install = await asyncio.create_subprocess_exec(
-            "pnpm", "install", stdout=asyncio.subprocess.DEVNULL
-        )
-        await install.wait()
+        if needs_install:
+            install = await asyncio.create_subprocess_exec("pnpm", "install")
+            await install.wait()
 
         start = await asyncio.create_subprocess_exec(
             "pnpm",
@@ -90,36 +97,43 @@ async def serve(
         host = None
         remote = None
         url = None
+        warnings = False
 
         while not (host and remote and url):
             line = (await start.stdout.readline()).decode("utf-8").rstrip()
-            # print(line)
+            print(line)
             if match := re.fullmatch(
                 r"layout start: <i> \[webpack-dev-server\] Loopback: (.+)", line
             ):
                 url = match.group(1)
             elif match := re.fullmatch(
-                r"(home|layout) start: webpack \d+\.\d+\.\d+ compiled successfully in \d+ ms",
+                r"(home|layout) start: webpack \d+\.\d+\.\d+ compiled (successfully|with \d+ warnings) in \d+ ms",
                 line,
             ):
                 if match.group(1) == "layout":
                     host = True
                 elif match.group(1) == "home":
                     remote = True
+                if match.group(2) != "successfully":
+                    warnings = True
 
-        result = {
-            "actual": await scrape(url),
-            "config": {
-                "host": {
-                    "package": host_package_version,
-                    "shared": host_shared_hints,
+        result = (
+            None
+            if warnings
+            else {
+                "actual": await scrape(url),
+                "config": {
+                    "host": {
+                        "package": host_package_version,
+                        "shared": host_shared_hints,
+                    },
+                    "remote": {
+                        "package": remote_package_version,
+                        "shared": remote_shared_hints,
+                    },
                 },
-                "remote": {
-                    "package": remote_package_version,
-                    "shared": remote_shared_hints,
-                },
-            },
-        }
+            }
+        )
         start.terminate()
         await start.wait()
         return result
@@ -134,21 +148,30 @@ async def serve(
 
 
 async def run(baseline):
-    count = 0
-
-    results = []
-
     newVersion = "^3.3.8"
     oldVersion = "^3.0.11"
 
+    runs_set = set()
+    runs_list = []
+
     # https://webpack.js.org/plugins/module-federation-plugin/#sharing-hints
-    for host_package_version in [newVersion, oldVersion]:
-        for remote_package_version in [newVersion, oldVersion]:
+    for host_package_version in [
+        newVersion,
+        newVersion.lstrip("^"),
+        oldVersion,
+        oldVersion.lstrip("^"),
+    ]:
+        for remote_package_version in [
+            newVersion,
+            newVersion.lstrip("^"),
+            oldVersion,
+            oldVersion.lstrip("^"),
+        ]:
             if (not baseline and host_package_version == remote_package_version) or (
                 baseline
                 and (
-                    host_package_version != newVersion
-                    or remote_package_version != newVersion
+                    host_package_version != remote_package_version
+                    or host_package_version not in [newVersion, newVersion.lstrip("^")]
                 )
             ):
                 continue
@@ -162,23 +185,12 @@ async def run(baseline):
                     {},
                     {},
                 ),
-                (
-                    {"version": host_package_version.lstrip("^")},
-                    {"version": remote_package_version.lstrip("^")},
-                ),
+                # https://webpack.js.org/plugins/module-federation-plugin/#version
+                # "By default, webpack uses the version from the package.json file of the dependency."
+                # Therefore we do not set version here explicitly...
                 (
                     {"requiredVersion": host_package_version},
                     {"requiredVersion": remote_package_version},
-                ),
-                (
-                    {
-                        "version": host_package_version.lstrip("^"),
-                        "requiredVersion": host_package_version,
-                    },
-                    {
-                        "version": remote_package_version.lstrip("^"),
-                        "requiredVersion": remote_package_version,
-                    },
                 ),
             ]:
                 for is_strict_version in [None, False, True]:
@@ -187,12 +199,15 @@ async def run(baseline):
                         or "requiredVersion" not in host_vue_shared
                     ):
                         continue
-                    for is_singleton in [None, False, True]:
+
+                    for is_singleton in [None, True]:
                         if is_singleton is not None and host_vue_shared is None:
                             continue
+
                         for is_import in [None, False]:
                             if is_import is not None and host_vue_shared is None:
                                 continue
+
                             host_shared = (
                                 {
                                     "vue": {
@@ -210,6 +225,7 @@ async def run(baseline):
                                 if host_vue_shared is not None
                                 else {}
                             )
+
                             remote_shared = (
                                 {
                                     "vue": {
@@ -229,41 +245,53 @@ async def run(baseline):
                                 else {}
                             )
 
-                            count += 1
-
-                            print(
-                                f"{count:03}",
-                                "|",
-                                "host package version:",
-                                host_package_version,
-                                "|",
-                                "remote package version:",
-                                remote_package_version,
-                                "|",
-                                "host shared:",
-                                json.dumps(host_shared),
-                                "|",
-                                "remote shared:",
-                                json.dumps(remote_shared),
-                                end="",
-                                flush=True,
-                            )
-
-                            result = await serve(
+                            args = [
                                 host_package_version,
                                 remote_package_version,
                                 host_shared,
                                 remote_shared,
-                            )
+                            ]
 
-                            if not result:
-                                print()
-                                raise Exception("No result!")
+                            args_json = json.dumps(args)
 
-                            print("", "|", "scraped result:", json.dumps(result))
-                            print()
+                            if args_json not in runs_set:
+                                runs_set.add(args_json)
+                                runs_list.append(args)
 
-                            results.append(result)
+    count = 0
+
+    old_install = None
+
+    results = []
+
+    for args in natsorted(runs_list, lambda args: f"{args[0]} {args[1]}"):
+        count += 1
+
+        print(
+            f"{count:03}",
+            "|",
+            "host package version:",
+            args[0],
+            "|",
+            "remote package version:",
+            args[1],
+            "|",
+            "host shared:",
+            json.dumps(args[2]),
+            "|",
+            "remote shared:",
+            json.dumps(args[3]),
+        )
+
+        result = await serve(*args, old_install != f"{args[0]} {args[1]}")
+
+        old_install = f"{args[0]} {args[1]}"
+
+        if result:
+            print(f"{count:03}", "|", "scraped result:", json.dumps(result))
+            print()
+
+            results.append(result)
 
     results_json = Path(
         "results_same_version.json" if baseline else "results_different_versions.json"
